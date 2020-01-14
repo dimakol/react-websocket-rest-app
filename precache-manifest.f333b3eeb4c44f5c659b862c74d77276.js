c8b3fd6d37b8c9b6c9e37e29b3ba4be1ee282fe2
Dima Kolyas <dimakol@gmail.com>
2020-01-14 23:32:39 +0200

    Updates

---
+++ precache-manifest.f333b3eeb4c44f5c659b862c74d77276.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4959aab4a999b462fb4b166244835e6c",
+    "url": "/WebSockets-REST_API/index.html"
+  },
+  {
+    "revision": "d1c3dad2978f03a92f83",
+    "url": "/WebSockets-REST_API/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b3598c50cc62db14bc8b",
+    "url": "/WebSockets-REST_API/static/css/main.78b65c14.chunk.css"
+  },
+  {
+    "revision": "d1c3dad2978f03a92f83",
+    "url": "/WebSockets-REST_API/static/js/2.62786d2d.chunk.js"
+  },
+  {
+    "revision": "b3598c50cc62db14bc8b",
+    "url": "/WebSockets-REST_API/static/js/main.d715a0c1.chunk.js"
+  },
+  {
+    "revision": "af47ef91c7e6784a9707",
+    "url": "/WebSockets-REST_API/static/js/runtime-main.de27eac6.js"
+  }
+]);
